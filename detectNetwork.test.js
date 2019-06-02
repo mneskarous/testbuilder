f2cb0885dc4465f1cf446a36aec6f9cb0ca7892a
Michael Eskarous <mneskarous@gmail.com>
2019-06-01 17:53:16 -0700

    Fixed Visa and MasterCard

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -8,7 +8,7 @@
 // different value.
 // var FILL_ME_IN = 'Fill this value in';
 //
-// describe('Introduction to Mocha Tests - READ ME FIRST', function() {
+describe('Introduction to Mocha Tests - READ ME FIRST', function() {
   // A Mocha test is just a function!
   // If the function throws an error when run, it fails.
   // If it doesn't throw an error when run, it doesn't fail.
@@ -17,37 +17,37 @@
   // Once you've read and understood this section, please comment it out.
   // You will not be able to proceed with a failing test.
 
-//   it('Throws an error so it fails', function() {
-//     throw new Error('Delete me!');
-//   });
-//
-//   it('Doesn\'t throw an error, so it doesn\'t fail', function() {
-//     // This test doesn't really test anything at all! It will pass no matter what.
-//     var even = function(num){
-//       return num/2 === 0;
-//     }
-//     return even(10) === true;
-//   });
-//
-//   // In tests, we want to compare the expected behavior to the actual behavior.
-//   // A test should only fail if the expected behavior doesn't match the actual.
-//   it('Throws an error when expected behavior does not match actual behavior', function() {
-//     var even = function(num){
-//       return num/2 === 0;
-//     }
-//
-//     if(even(10) !== true) {
-//       throw new Error('10 should be even!');
-//     }
-//   });
-// });
+  it('Throws an error so it fails', function() {
+    throw new Error('Delete me!');
+  });
+
+  it('Doesn\'t throw an error, so it doesn\'t fail', function() {
+    // This test doesn't really test anything at all! It will pass no matter what.
+    var even = function(num){
+      return num/2 === 0;
+    }
+    return even(10) === true;
+  });
+
+  // In tests, we want to compare the expected behavior to the actual behavior.
+  // A test should only fail if the expected behavior doesn't match the actual.
+  it('Throws an error when expected behavior does not match actual behavior', function() {
+    var even = function(num){
+      return num/2 === 0;
+    }
+
+    if(even(10) !== true) {
+      throw new Error('10 should be even!');
+    }
+  });
+});
 describe('Diner\'s Club', function() {
   // Be careful, tests can have bugs too...
 
   it('has a prefix of 38 and a length of 14', function() {
     throw new Error('Delete me!');
 
-    if (detectNetwork('38345678901234') !== 'Diner\'s Club') {
+    if (detectNetwork('383456789012345') !== 'Diner\'s Club') {
       throw new Error('Test failed');
     }
   });
@@ -84,7 +84,7 @@ describe('Visa', function() {
   // Chai provides an assert that acts the same as our previous assert.
   // Search the documentation to figure out how to access it.
   //   http://chaijs.com/
-  var assert = chai.FILL_ME_IN;
+  var assert = chai.assert;
 
 
   it('has a prefix of 4 and a length of 13', function() {
@@ -107,15 +107,15 @@ describe('MasterCard', function() {
   //   http://chaijs.com/api/bdd/
   var expect = chai.expect;
 
-  it(FILL_ME_IN, function() {
+  it('has a prefix of 51 and a length of 16', function() {
     expect(detectNetwork('5112345678901234')).to.equal('MasterCard');
   });
 
-  it(FILL_ME_IN, function() {
+  it('has a prefix of 52 and a length of 16', function() {
     expect(detectNetwork('5212345678901234')).to.equal('MasterCard');
   });
 
-  it(FILL_ME_IN, function() {
+  it('has a prefix of 53 and a length of 16', function() {
     expect(detectNetwork('5312345678901234')).to.equal('MasterCard');
   });
 
@@ -130,11 +130,11 @@ describe('MasterCard', function() {
   var should = chai.should();
 
   it('has a prefix of 54 and a length of 16', function() {
-    detectNetwork('5412345678901234').should.equal(FILL_ME_IN);
+    detectNetwork('5412345678901234').should.equal('MasterCard');
   });
 
   it('has a prefix of 55 and a length of 16', function() {
-    detectNetwork('5512345678901234').should.equal(FILL_ME_IN);
+    detectNetwork('5512345678901234').should.equal('MasterCard');
   })
 
 });
